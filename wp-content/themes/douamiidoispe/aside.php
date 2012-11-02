bfa9168dbcab13439f0ae4bf21464e2ee0703b05
Norbert <norbiu@gmail.com>
2012-11-02 13:11:44 +0200

    Revert "Modificari din terminal"
    This reverts commit 6a9984821a96e50fc9a68c24e23872025c14fc64.

--- wp-content/themes/douamiidoispe/aside.php
+++ wp-content/themes/douamiidoispe/aside.php
@@ -15,7 +15,7 @@
 			<hgroup>
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 				<h3 class="entry-format"><?php _e( 'Aside', 'twentyeleven' ); ?></h3>
-			</hgroup>qwqweqweqw
+			</hgroup>
 
 			<?php if ( comments_open() && ! post_password_required() ) : ?>
 			<div class="comments-link">
@@ -27,7 +27,7 @@
 		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->qweqweq
+		</div><!-- .entry-summary -->
 		<?php else : ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
